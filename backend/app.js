8799552c82db362e781f962e9c693c6c3f5c0116
Gyanshu-kumar <gyanshusingh152@gmail.com>
2024-03-20 10:40:19 +0530

    commit

--- backend/app.js
+++ backend/app.js
@@ -4,34 +4,28 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const cloudinary = require("cloudinary").v2; // Import Cloudinary
-const path = require("path");
 
-
-// Configure Cloudinary
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
-});
-
-// Middleware
 app.use(cors({
-  origin: 'https://g-mart-23zd.vercel.app',
+  origin: ['https://g-mart-23zd.vercel.app',],
   credentials: true
 }));
+
 app.use(express.json());
 app.use(cookieParser());
-app.use(bodyParser.json({ limit: '100mb' }));
-app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
-app.use("/", express.static(path.join(__dirname, "./uploads")));
-
-
-// Routes
 app.use("/test", (req, res) => {
   res.send("Hello world!");
 });
-// Import routes
+
+app.use(bodyParser.urlencoded({ extended: true, limit: "100mb" }));
+
+// config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({
+    path: "config/.env",
+  });
+}
+
+// import routes
 const user = require("./controller/user");
 const shop = require("./controller/shop");
 const product = require("./controller/product");
@@ -43,7 +37,6 @@ const conversation = require("./controller/conversation");
 const message = require("./controller/message");
 const withdraw = require("./controller/withdraw");
 
-// Use routes
 app.use("/api/v2/user", user);
 app.use("/api/v2/conversation", conversation);
 app.use("/api/v2/message", message);
@@ -55,7 +48,7 @@ app.use("/api/v2/coupon", coupon);
 app.use("/api/v2/payment", payment);
 app.use("/api/v2/withdraw", withdraw);
 
-// Error handling middleware
+// it's for ErrorHandling
 app.use(ErrorHandler);
 
 module.exports = app;
