16be5a4676b899b35df4250a71622a1e3004f6eb
Gyanshu-kumar <gyanshusingh152@gmail.com>
2024-03-23 14:46:18 +0530

    commit

--- frontend/src/components/Signup/Signup.jsx
+++ frontend/src/components/Signup/Signup.jsx
@@ -148,6 +148,7 @@ const Singup = () => {
                   className="ml-5 flex items-center justify-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50"
                 >
                   <span>Upload a file</span>
+                  <p1>Less than 50 kb</p1>
                   <input
                     type="file"
                     name="avatar"
