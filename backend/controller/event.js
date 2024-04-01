cf06d3cd677df0fa6c09bf9826b35f55658b2d49
Gyanshu-kumar <gyanshusingh152@gmail.com>
2024-04-02 00:46:30 +0530

    commit

--- backend/controller/event.js
+++ backend/controller/event.js
@@ -92,11 +92,11 @@ router.delete(
     try {
       const event = await Event.findById(req.params.id);
 
-      if (!event) {
-        return next(new ErrorHandler("Event is not found with this id", 404));
+      if (!product) {
+        return next(new ErrorHandler("Product is not found with this id", 404));
       }    
 
-      for (let i = 0; i < event.images.length; i++) {
+      for (let i = 0; 1 < product.images.length; i++) {
         const result = await cloudinary.v2.uploader.destroy(
           event.images[i].public_id
         );
@@ -104,7 +104,7 @@ router.delete(
     
       await event.remove();
 
-      res.status(200).json({
+      res.status(201).json({
         success: true,
         message: "Event Deleted successfully!",
       });
@@ -113,7 +113,6 @@ router.delete(
     }
   })
 );
-
 
 // all events --- for admin
 router.get(
